2d1cc6c1cea74a7f76fb03430f1a793239a716c5
nomba <nompo4ta@gmail.com>
2024-11-10 17:26:24 +0300

    `BackgroundJobs` adapter

---
+++ DeliveryApp.Api/Adapters/BackgroundJobs/AssignOrdersJob.cs
@@ -0,0 +1,27 @@
+﻿using DeliveryApp.Core.Application.UseCases.Commands.AssignOrder;
+using MediatR;
+using Quartz;
+
+namespace DeliveryApp.Api.Adapters.BackgroundJobs;
+
+/*
+ * Распределение и назначение заказов на курьеров (раз в 1 секунду)
+ */
+
+[DisallowConcurrentExecution]
+public class AssignOrdersJob : IJob
+{
+    private readonly IMediator _mediator;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public AssignOrdersJob(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var assignOrdersCommand = new AssignOrderCommand();
+        await _mediator.Send(assignOrdersCommand);
+    }
+}
--- DeliveryApp.Api/Program.cs
+++ DeliveryApp.Api/Program.cs
@@ -1,3 +1,6 @@
+using DeliveryApp.Infrastructure.Adapters.Postgres;
+using Microsoft.EntityFrameworkCore;
+
 namespace DeliveryApp.Api;
 
 public class Program
@@ -5,10 +8,14 @@ public class Program
     public static void Main(string[] args)
     {
         var host = CreateHostBuilder(args).Build();
+        
+        // Apply Migrations on app start
         using (var scope = host.Services.CreateScope())
         {
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
         }
-
+        
         host.Run();
     }
 
