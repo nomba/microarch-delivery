b532fa151301336716ffde03a8c136700dc9698f
nomba <nompo4ta@gmail.com>
2024-11-16 11:14:09 +0300

    Introduce `IGeoClient` port

--- Tests/DeliveryApp.UnitTests/Application/CreateOrderCommandShould.cs
+++ Tests/DeliveryApp.UnitTests/Application/CreateOrderCommandShould.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
 using DeliveryApp.Core.Application.UseCases.Commands.CreateOrder;
 using DeliveryApp.Core.Domain.Model.OrderAggregate;
 using DeliveryApp.Core.Domain.SharedKernel;
@@ -15,12 +16,15 @@ namespace DeliveryApp.UnitTests.Application;
 public class CreateOrderCommandShould
 {
     private readonly IOrderRepository _orderRepositoryMock;
-    private readonly IUnitOfWork _unitOfWork;
-    
+    private readonly IUnitOfWork _unitOfWorkMock;
+    private readonly IGeoClient _geoClientMock;
+
+    // ReSharper disable once ConvertConstructorToMemberInitializers
     public CreateOrderCommandShould()
     {
         _orderRepositoryMock = Substitute.For<IOrderRepository>();
-        _unitOfWork = Substitute.For<IUnitOfWork>();
+        _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+        _geoClientMock = Substitute.For<IGeoClient>();
     }
 
     [Fact]
@@ -31,11 +35,12 @@ public class CreateOrderCommandShould
         var orderId = Guid.NewGuid();
         var alreadyCreatedOrder = Order.Create(orderId, Location.Create(1, 1).Value).Value;
         _orderRepositoryMock.GetById(Arg.Any<Guid>()).Returns(Task.FromResult(alreadyCreatedOrder));
+        _geoClientMock.GetLocation(Arg.Any<string>()).Returns(Task.FromResult(Location.Create(1, 1)));
         
         // Act
         
         var command = new CreateOrderCommand(orderId, "Test Street");
-        var handler = new CreateOrderCommandHandler(_orderRepositoryMock, _unitOfWork);
+        var handler = new CreateOrderCommandHandler(_orderRepositoryMock, _unitOfWorkMock, _geoClientMock);
         var result = await handler.Handle(command, CancellationToken.None);
 
         // Assert
@@ -43,7 +48,8 @@ public class CreateOrderCommandShould
         result.Should().BeTrue();
         await _orderRepositoryMock.Received(1).GetById(Arg.Is(orderId));
         await _orderRepositoryMock.Received(0).Add(Arg.Any<Order>());
-        await _unitOfWork.Received(0).SaveEntities();
+        await _unitOfWorkMock.Received(0).SaveEntities();
+        await _geoClientMock.Received(0).GetLocation(Arg.Any<string>());
     }
 
     
@@ -52,19 +58,22 @@ public class CreateOrderCommandShould
     {
         // Arrange
         
+         const string street = "Test Street";
         _orderRepositoryMock.GetById(Arg.Any<Guid>()).Returns(Task.FromResult((Order) null));
-        _unitOfWork.SaveEntities().Returns(Task.FromResult(true));
-
+        _unitOfWorkMock.SaveEntities().Returns(Task.FromResult(true));
+        _geoClientMock.GetLocation(Arg.Any<string>()).Returns(Task.FromResult(Location.Create(1, 1)));
+        
         // Act
         
-        var command = new CreateOrderCommand(Guid.NewGuid(), "Test Street");
-        var handler = new CreateOrderCommandHandler(_orderRepositoryMock, _unitOfWork);
+        var command = new CreateOrderCommand(Guid.NewGuid(), street);
+        var handler = new CreateOrderCommandHandler(_orderRepositoryMock, _unitOfWorkMock, _geoClientMock);
         var result = await handler.Handle(command, CancellationToken.None);
 
         // Assert
         
         result.Should().BeTrue();
         await _orderRepositoryMock.Received(1).Add(Arg.Any<Order>());
-        await _unitOfWork.Received(1).SaveEntities();
+        await _unitOfWorkMock.Received(1).SaveEntities();
+        await _geoClientMock.Received(1).GetLocation(street);
     }
 }
